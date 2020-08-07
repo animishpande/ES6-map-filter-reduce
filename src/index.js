eeef1209b83336434738f99d4834ede19040beec
Animish Pande <63346282+animishpande@users.noreply.github.com>
2020-08-08 00:32:13 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+var numbers = [3, 56, 2, 48, 5];
+
+//Map -Create a new array by doing something with each item in an array.
+
+// var newNumbers = numbers.map(function (x){
+//   return x * 2;
+// });
+// console.log(newNumbers);
+
+//Filter - Create a new array by keeping the items that return true.
+
+// var newNumbers = numbers.filter(function (num) {
+//   return num < 10;
+// });
+
+// console.log(newNumbers);
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+
+// var newNumber = numbers.reduce(function (accumulator, currentNum) {
+//   console.log("accumulator = "+accumulator);
+// console.log("currentNum = "+currentNum);
+//   return accumulator + currentNum;
+// });
+// console.log(newNumber);
+
+//Find - find the first item that matches from an array.
+
+// var newNumber = numbers.find(function (num) {
+//   return num > 10;
+// });
+// console.log(newNumber);
+
+//FindIndex - find the index of the first item that matches.
+
+// var newNumber = numbers.findIndex(function (num) {
+//   return num > 10;
+// });
+// console.log(newNumber);
